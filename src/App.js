c3945059f3999abfb2c5ec064b41aed91776f2c6
rockpell <aythffk@gmail.com>
2020-01-11 22:24:31 +0900

    javascript 내에서 객체 처리하는 코드

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramovm, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component{
   render(){
-    const helloWorld = 'Hello React!!!';
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        {list.map(function(item){
+        return (
+          <div key={item.objectID}>
+            <sapn>
+              <a href={item.url}>{item.title}</a>
+            </sapn>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <sapn>{item.points}</sapn>
+          </div>
+          );
+        })}
       </div>
     );
   }
