bc73e3f81176107eb046975ed16492c8ac82b233
rockpell <aythffk@gmail.com>
2020-01-11 22:06:27 +0900

    일정 간격으로 현재 시간을 렌더링하는 테스트 코드

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+function tick(){
+    const element = (
+        <div>
+            <h2>Hello, world!!!</h2>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
