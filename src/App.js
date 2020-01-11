585767558f6b16eec34963aecabd2611e83c2e40
rockpell <aythffk@gmail.com>
2020-01-11 21:26:29 +0900

    var 변수 const 변수로 변경

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 class App extends Component{
   render(){
-    var helloWorld = 'Hello React!!!';
+    const helloWorld = 'Hello React!!!';
     return (
       <div className="App">
         <h2>{helloWorld}</h2>
