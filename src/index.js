9cf85bb8b566aa19790d783f6ae10fa3731a55e8
rockpell <aythffk@gmail.com>
2020-01-11 22:13:02 +0900

    HMR 코드 추가

--- src/index.js
+++ src/index.js
@@ -2,23 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-function tick(){
-    const element = (
-        <div>
-            <h2>Hello, world!!!</h2>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
+// leanpub-start-insert
+// Hot Module Replacement: 브라우저 내 애플리케이션을 재실행하는 도구
+// 아래의 코드를 추가하면 코드를 수정해도 브라우저 페이지가 새로고침 되지 않고 어플리케이션이 재실행된다.
+if(module.hot){
+    module.hot.accept();
 }
+// leanpub-end-insert
 
-setInterval(tick, 1000);
+// function tick(){
+//     const element = (
+//         <div>
+//             <h2>Hello, world!!!</h2>
+//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//         </div>
+//     );
+//     ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000);
