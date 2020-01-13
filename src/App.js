5761f57272809be7cb143b3a7410524f90cb9b27
rockpell <aythffk@gmail.com>
2020-01-13 11:39:09 +0900

    state 코드 추가

--- src/App.js
+++ src/App.js
@@ -19,34 +19,22 @@ const list = [
   },
 ];
 
-// 화살표 함수 사용
-// class App extends Component{
-//   render(){
-//     return (
-//       <div className="App">
-//         {list.map(item => {
-//         return (
-//           <div key={item.objectID}>
-//             <sapn>
-//               <a href={item.url}>{item.title}</a>
-//             </sapn>
-//             <span>{item.author}</span>
-//             <span>{item.num_comments}</span>
-//             <sapn>{item.points}</sapn>
-//           </div>
-//           );
-//         })}
-//       </div>
-//     );
-//   }
-// }
-
 // 화살표 함수는 중괄호를 생략하면 자동으로 return 명령이 추가된다.
 class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = { 
+      list: list, // list를 컴포넌트 내부 상태로 정의
+    };
+  }
+  // 상태가 변경 될 때마다 render가 재실행된다.
+  // state는 setState() 메서드를 통해서 변경해야한다.
   render(){
-    return (
+    return ( 
       <div className="App">
-        {list.map(item =>
+        {this.state.list.map(item =>
           <div key={item.objectID}>
             <sapn>
               <a href={item.url}>{item.title}</a>
